192d6361160d342daf10951fca1e197f99ff1969
Philip Chen <philipandrewchen@gmail.com>
2021-09-01 01:39:33 -0500

    dffv

--- index.js
+++ index.js
@@ -98,11 +98,13 @@ app.delete('/api/:userId/:itemId', (req, res) => {
     deleteItem(req, res, req.params.userId, req.params.itemId)
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname+"client/build/index.html"));
-  });
 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+      });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
