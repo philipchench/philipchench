467fa1e13c2c1295b19c82565a2ec701131188d5
Philip Chen <philipandrewchen@gmail.com>
2021-09-01 03:13:25 -0500

    aefaef

--- index.js
+++ index.js
@@ -106,5 +106,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build'))
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
