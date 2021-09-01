aed90e973af4db3e84f03ea593c087cf70072e08
Philip Chen <philipandrewchen@gmail.com>
2021-09-01 03:02:21 -0500

    ascasc

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 
-const uri = 'mongodb+srv://pinusmacbook:pinusmacbook@cluster0.iblwx.mongodb.net/ShoppingListProj?retryWrites=true&w=majority'
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb+srv://pinusmacbook:pinusmacbook@cluster0.iblwx.mongodb.net/ShoppingListProj?retryWrites=true&w=majority'
 
-mongoose.connect(uri || process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error(err))
 
